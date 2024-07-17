486bb94b53c6206728c7ce452b3a79e4c96b4e06
Joshua Bergeron <pacluc20@aol.com>
2024-07-16 20:49:16 -0700

    clean up test file

--- src/components/VehicleList.test.js
+++ src/components/VehicleList.test.js
@@ -1,37 +1,10 @@
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import VehicleList from "./VehicleList";
-
-const mockData = [
-  {
-    make: "Toyota",
-    model: "Corolla",
-    year: "2003",
-    mileage: 178234,
-    color: "White",
-    lastServiceDate: "05/16/2024",
-    lastServiceType: "Oil Change",
-  },
-  {
-    make: "BMW",
-    model: "Z4",
-    year: "2008",
-    mileage: 84772,
-    color: "Black",
-  },
-  {
-    make: "Honda",
-    model: "Civic Type R",
-    year: "2023",
-    mileage: 2102,
-    color: "Blue",
-    lastServiceDate: "07/22/2024",
-    lastServiceType: "New Tires",
-  },
-];
+import { vehicleList } from "@/mocks/vehicleMocks";
 
 it("renders each vehicle summary item as a list", () => {
-  render(<VehicleList vehicleList={mockData} />);
+  render(<VehicleList vehicleList={vehicleList} />);
 
   expect(screen.getByText("Toyota Corolla")).toBeInTheDocument();
   expect(screen.getByText("2003 - White")).toBeInTheDocument();
@@ -48,7 +21,7 @@ it("renders each vehicle summary item as a list", () => {
   expect(screen.getByText("2023 - Blue")).toBeInTheDocument();
   expect(screen.getByText("Mileage: 2,102 miles")).toBeInTheDocument();
   expect(
-    screen.getByText("Last Service: 07/22/2024 - New Tires")
+    screen.getByText("Last Service: 07/22/2024 - New Air Filter")
   ).toBeInTheDocument();
 
   expect(screen.queryAllByTestId("maintenance-button")).toHaveLength(3);
