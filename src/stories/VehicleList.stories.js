19dc9c47d7be26f83647c730158c90a293c22a6d
Joshua Bergeron <pacluc20@aol.com>
2024-07-16 20:03:12 -0700

    Add vehicle list component

---
+++ src/stories/VehicleList.stories.js
@@ -0,0 +1,53 @@
+import VehicleList from "@/components/VehicleList";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: "Vehicle List",
+  component: VehicleList,
+  // parameters: {
+  //   layout: "centered",
+  // },
+  // tags: ["autodocs"],
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const WithMultipleVehicles = {
+  args: {
+    vehicleList: [
+      {
+        make: "Toyota",
+        model: "Corolla",
+        year: "2003",
+        mileage: 178234,
+        color: "White",
+        lastServiceDate: "05/16/2024",
+        lastServiceType: "Oil Change",
+      },
+      {
+        make: "BMW",
+        model: "Z4",
+        year: "2008",
+        mileage: 84772,
+        color: "Black",
+        lastServiceDate: "01/23/2023",
+        lastServiceType: "New Tires",
+      },
+      {
+        make: "Honda",
+        model: "Civic Type R",
+        year: "2023",
+        mileage: 2102,
+        color: "Blue",
+        lastServiceDate: "07/22/2024",
+        lastServiceType: "New Air Filter",
+      },
+    ],
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const WithNoVehicles = {
+  args: {
+    vehicleList: [],
+  },
+};
