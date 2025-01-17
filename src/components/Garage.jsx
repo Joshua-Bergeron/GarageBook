1174ba7c85a51dff9dc7c1e85e08ff4e108b88fd
Joshua Bergeron <pacluc20@aol.com>
2025-01-16 19:34:42 -0800

    add new vehicle button modal

--- src/components/Garage.jsx
+++ src/components/Garage.jsx
@@ -1,17 +1,43 @@
+"use client";
 import * as React from "react";
 import { Box, Container } from "@mui/material";
 import VehicleList from "./VehicleList";
 import GarageHeader from "./GarageHeader";
 import NavigationBar from "./NavigationBar";
+import NewVehicleForm from "./NewVehicleForm";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 function Garage({ vehicleList }) {
+  const [showNewVehicleForm, setShowNewVehicleForm] = React.useState(false);
+
+  const handleNewVehicleClick = () => {
+    setShowNewVehicleForm(true);
+  };
+
+  const handleClose = () => {
+    setShowNewVehicleForm(false);
+  };
+
   return (
     <>
       <NavigationBar />
       <Container sx={{ paddingTop: 4 }}>
-        <GarageHeader />
+        <GarageHeader onNewVehicleClick={handleNewVehicleClick} />
         <VehicleList vehicleList={vehicleList} />
       </Container>
+      <Dialog
+        open={showNewVehicleForm}
+        onClose={handleClose}
+        fullWidth
+        maxWidth="md"
+      >
+        <DialogTitle>Add New Vehicle</DialogTitle>
+        <DialogContent>
+          <NewVehicleForm onCancel={handleClose} />
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
